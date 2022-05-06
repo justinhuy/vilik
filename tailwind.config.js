5c45c3e42983ebbd9f7c86e1d62c60ebe89b0258
Anh Nguyen <anhnguyen@Anhs-MacBook-Pro.local>
2022-05-06 08:00:02 +0700

    home ver 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        "zinc-50": "#FAFAFA",
         "zinc-100": "#F4F4F5",
+        "zinc-200": "#E4E4E7",
         "zinc-300": "#D4D4D8",
+        "zinc-400": "#A1A1AA",
         "zinc-500": "#71717A",
+        "zinc-600": "#52525B",
+        "zinc-800": "#27272A;",
+        "zinc-900": "#18181B",
+        "light-blue": "#00D1FF",
       },
     },
   },
