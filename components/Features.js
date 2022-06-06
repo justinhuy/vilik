210bffb345f0cd9d8c8b4818d16a83a3b85a1705
Anh Nguyen <anhnguyen@Anhs-MacBook-Pro.local>
2022-06-06 09:01:46 +0700

    fix missing e

--- components/Features.js
+++ components/Features.js
@@ -51,30 +51,29 @@ const Features = ({ activeSection }) => {
           </div>
 
           <div className="hidden lg:block absolute w-[477px] right-[-28px] top-1/2 -translate-y-2/4 z-10">
-            {activeSection === 2 ||
-              (inView ? (
-                <Moving yFrom={-5} yTo={5} duration={3}>
-                  <img
-                    src="/earth-layer-3.jpg"
-                    className="w-full h-auto opacity-0"
-                  />
+            {activeSection === 2 || inView ? (
+              <Moving yFrom={-5} yTo={5} duration={3}>
+                <img
+                  src="/earth-layer-3.jpg"
+                  className="w-full h-auto opacity-0"
+                />
 
-                  <div className="w-[93.2914046%] h-auto absolute top-0 left-[2.51572327%]">
-                    <Moving rotateTo={360} duration={60}>
-                      <img src="/e.png" className="w-full h-auo" />
-                    </Moving>
-                  </div>
+                <div className="w-[93.2914046%] h-auto absolute top-0 left-[2.51572327%]">
+                  <Moving rotateTo={360} duration={60}>
+                    <img src="/e.png" className="w-full h-auo" />
+                  </Moving>
+                </div>
 
-                  <img
-                    src="/line-1.png"
-                    className="absolute w-[72.327044%] h-auto top-[22.4719101%] left-[14.6750524%]"
-                  />
-                  <img
-                    src="/line-2.png"
-                    className="absolute w-[81.7610063%] h-auto top-[22.4719101%] left-[9.43396226%]"
-                  />
-                </Moving>
-              ) : null)}
+                <img
+                  src="/line-1.png"
+                  className="absolute w-[72.327044%] h-auto top-[22.4719101%] left-[14.6750524%]"
+                />
+                <img
+                  src="/line-2.png"
+                  className="absolute w-[81.7610063%] h-auto top-[22.4719101%] left-[9.43396226%]"
+                />
+              </Moving>
+            ) : null}
           </div>
         </div>
 
